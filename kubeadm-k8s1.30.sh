2cf19e968a84f548d7f78ec9fac1e305e96a0a03
Alexander <120117037+AlexChronicles@users.noreply.github.com>
2024-10-10 10:40:19 +0300

    Update kubeadm-k8s1.31.sh

--- kubeadm-k8s1.30.sh
+++ kubeadm-k8s1.30.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 
 #Addition kube 1.31 repo. If u want to change version, just replace package version in URL string
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Installing Kubernates services
